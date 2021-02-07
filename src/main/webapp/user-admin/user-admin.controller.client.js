8c8d858695d69e946c3b1ebe8dda86743e8734fb
Jason Miller <jasonmiller@Jasons-MacBook-Pro.local>
2021-02-07 13:33:27 -0700

    services created. cleaning up user-admin js

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,19 +1,48 @@
-var addUserBtn
+var $usernameFld
+var $passwordFld
+var $firstNameFld
+var $lastNameFld
+var $roleFld
+var $addUserBtn
+var $tableBody
+
 var users = [
-    {username: 'webbmcfrogg4', password: '1cozylilypad', firstname: 'Webb', lastname: 'McFroggington', role: 'FACULTY'},
+    /*{username: 'webbmcfrogg4', password: '1cozylilypad', firstname: 'Webb', lastname: 'McFroggington', role: 'FACULTY'},
     {username: 'froggy', password: 'froggy2', firstname: 'Sir', lastname: 'Froggy', role: 'FACULTY'},
-    {username: 'froggy', password: 'froggy2', firstname: 'Sir', lastname: 'Froggy', role: 'FACULTY'}
+    {username: 'froglady', password: 'ladybug', firstname: 'Madame', lastname: 'Froggy', role: 'FACULTY'}*/
 ]
 
-console.log(users)
+function addUser() {
+    createUser({
+        username: $usernameFld.val(),
+        password: $passwordFld.val(),
+        firstname: $firstNameFld.val(),
+        lastname: $lastNameFld.val(),
+        role: $roleFld.val()
+    })
+    $usernameFld.val("")
+    $passwordFld.val("")
+    $firstNameFld.val("")
+    $lastNameFld.val("")
+}
 
-var tableBody = $("tbody")
+function createUser(user) {
+    users.push(user)
+    renderUsers(users)
+}
+
+function deleteCourse(event) {
+    var deleteBtn = $(event.currentTarget)
+    var theID = deleteBtn.attr("id")
+    users.splice(theID, 1)
+    renderUsers(users)
+}
 
 function renderUsers(users) {
-    tableBody.empty()
+    $tableBody.empty()
     for (var i = 0; i < users.length; i++) {
         var user = users[i]
-        tableBody
+        $tableBody
             .prepend(`
             <tr>
                 <td>${user.username}</td>
@@ -22,7 +51,8 @@ function renderUsers(users) {
                 <td>${user.lastname}</td>
                 <td>${user.role}</td>
                 <td class="text-right">
-                    <button class="webb-btn-textonly">
+                    <button class="webb-btn-delete-user webb-btn-textonly"
+                            id="${i}">
                         <i class="fa-2x fa fa-times"></i>
                     </button>
                     <button class="webb-btn-textonly">
@@ -32,34 +62,33 @@ function renderUsers(users) {
             </tr>
         `)
     }
+    $(".webb-btn-delete-user")
+        .click(deleteCourse)
 }
-renderUsers(users)
 
 function init() {
-    var addUserBtn = $(".webb-btn-create-user")
-}
+    $usernameFld = $(".webb-username-fld")
+    $passwordFld = $(".webb-password-fld")
+    $firstNameFld = $(".webb-first-name-fld")
+    $lastNameFld = $(".webb-last-name-fld")
+    $roleFld = $(".webb-role-fld")
+    $addUserBtn = $(".webb-btn-create-user")
+    $tableBody = $("tbody")
 
-/*
-function renderCourses(courses) {
-    theTableBody.empty()
-    for (var i = 0; i < courses.length; i++) {
-        var course = courses[i]
-        theTableBody
-            .prepend(`
-    <tr>
-        <td>${course.title}</td>
-        <td>${course.seats}</td>
-        <td>${course.semester}</td>
-        <td>
-            <button class="wbdv-delete" id="${i}">Delete</button>
-            <button>Select</button>
-        </td>
-    </tr>
-  `)
-    }
-    jQuery(".wbdv-delete")
-        .click(function (event) {
-            console.log(event.target)
-        })
+    $addUserBtn.click(() => {
+            createUser({
+                username: $usernameFld.val(),
+                password: $passwordFld.val(),
+                firstname: $firstNameFld.val(),
+                lastname: $lastNameFld.val(),
+                role: $roleFld.val()
+            })
+            $usernameFld.val("")
+            $passwordFld.val("")
+            $firstNameFld.val("")
+            $lastNameFld.val("")
+        }
+    )
 }
-renderCourses(courses)*/
+$(init)
+
